5c43160a5bfad1374a82c7fa8b39789aa03ae5f6
Luke Whitten <whittenluke@gmail.com>
2025-02-22 18:49:56 -0500

    undo

--- src/app/auth/callback/route.ts
+++ src/app/auth/callback/route.ts
@@ -1,16 +1,1 @@
-import { createRouteHandlerClient } from '@supabase/auth-helpers-nextjs'
-import { cookies } from 'next/headers'
-import { NextResponse } from 'next/server'
-import type { Database } from '@/types/supabase'
-
-export async function GET(request: Request) {
-  const requestUrl = new URL(request.url)
-  const code = requestUrl.searchParams.get('code')
-
-  if (code) {
-    const supabase = createRouteHandlerClient<Database>({ cookies })
-    await supabase.auth.exchangeCodeForSession(code)
-  }
-
-  return NextResponse.redirect(requestUrl.origin)
-} 
+// Removing all the added code - returning to empty file 
